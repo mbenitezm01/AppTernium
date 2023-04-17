5270cf9afbed9f4b52d6db67781e4a382b8111e4
Eduardo1478 <jepf2001@hotmail.com>
2023-04-17 11:06:41 -0600

    Agregue todos los filtros

--- src/components/pages/Busqueda/Sidebar.js
+++ src/components/pages/Busqueda/Sidebar.js
@@ -12,12 +12,19 @@ function Sidebar(){
             </div>
             <div className='filter-box'>
                 <Form>
-                    <Form.Group>
+                    <Form.Group className='filters'>
                         <Form.Label>Nombre</Form.Label>
                         <Form.Control type="nombre" placeholder="Buscar" />
                     </Form.Group>
 
-                    <Form.Group style={{display:'inline-block'}}>
+
+                    <Form.Group className='filters'> 
+                        <Form.Label>CET    </Form.Label>
+                        <Form.Control type="cet" placeholder="Buscar" />
+                    </Form.Group>
+
+
+                    <Form.Group  className='filters' style={{display:'inline-block'}}>
                         <Form.Label>Antigüedad</Form.Label>
                         <Row style={{display:'flex'}}>
                         <Col style={{marginRight: '20px'}}>
@@ -27,12 +34,23 @@ function Sidebar(){
                         <Form.Control type="antiguedad" placeholder="Max" style={{width: '50px'}}/>
                             </Col>
                         </Row>
-                            
+                    </Form.Group>
+
+
+                    <Form.Group  className='filters' style={{display:'inline-block'}}>
+                        <Form.Label>Calificacion</Form.Label>
+                        <Row style={{display:'flex'}}>
+                        <Col style={{marginRight: '20px'}}>
+                        <Form.Control type="antiguedad" placeholder="Min" style={{width: '50px'}}/>
+                            </Col>
+                            <Col>
+                        <Form.Control type="antiguedad" placeholder="Max" style={{width: '50px'}}/>
+                            </Col>
+                        </Row>
                     </Form.Group>
 
                     
-
-                    <Form.Group>
+                    <Form.Group className='filters'>
                         <Form.Label>Estructura 3</Form.Label>
                         <Form.Select aria-label="Default select example">
                             <option>Open this select menu</option>
@@ -42,7 +60,8 @@ function Sidebar(){
                         </Form.Select>
                     </Form.Group>
 
-                    <Form.Group>
+
+                    <Form.Group className='filters'>
                         <Form.Label>Estructura 4</Form.Label>
                         <Form.Select aria-label="Default select example">
                             <option>Open this select menu</option>
@@ -52,22 +71,62 @@ function Sidebar(){
                         </Form.Select>
                     </Form.Group>
 
-                    <Form.Group>
-                        <Form.Label>Puesto</Form.Label>
-                        <Form.Control type="antiguedad" placeholder="Password" />
+
+                    <Form.Group className='filters'>
+                        <Form.Label>Performance</Form.Label>
+                        <Form.Select aria-label="Default select example">
+                            <option>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </Form.Select>
                     </Form.Group>
 
-                    <Form.Group>
+
+                    <Form.Group className='filters'>
+                        <Form.Label>Jefe</Form.Label>
+                        <Form.Select aria-label="Default select example">
+                            <option>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </Form.Select>
+                    </Form.Group>
+
+
+                    <Form.Group className='filters'>
+                        <Form.Label>Potencial</Form.Label>
+                        <Form.Select aria-label="Default select example">
+                            <option>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </Form.Select>
+                    </Form.Group>
+
+
+                    <Form.Group className='filters'>
+                        <Form.Label>Puesto</Form.Label>
+                        <Form.Select aria-label="Default select example">
+                            <option>Open this select menu</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </Form.Select>
+                    </Form.Group>
+
+
+                    <Form.Group className='filters'>
                         <Form.Label>Key Talent</Form.Label>
                         <Form.Check type="checkbox" placeholder="Password" />
                     </Form.Group>
+
 
                     <Button variant="primary" type="submit">
                         Submit
                     </Button>
                 </Form>
-
-                
+           
 
             </div>
         </div>
