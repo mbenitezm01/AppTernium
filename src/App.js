f79b8d64494672d7c2a7ef3a7537dcfa47f879fe
RKipper2000 <A01283516@itesm.mx>
2023-04-23 12:38:29 -0600

    login v1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header';
 import Busqueda from './components/pages/Busqueda/Busqueda';
 import Ficha from './components/pages/Ficha/Ficha';
 import Usuarios from './components/pages/Usuarios/Usuarios';
+import Login from './components/pages/Login/Login'
 
 //Imports de los componentes de las paginas
 
@@ -20,6 +21,7 @@ function App() {
           <Route><Route path='/busqueda' element={<Busqueda />} /></Route>
           <Route><Route path='/ficha/:id' element={<Ficha />} /></Route>
           <Route><Route path='/usuarios' element={<Usuarios />} /></Route>
+          <Route><Route path='/login' element={<Login />} /></Route>
         </Routes>
       </BrowserRouter>
     </div>
