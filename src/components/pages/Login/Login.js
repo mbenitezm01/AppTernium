f7b227e99fd105fff575848f20be9bf0516cced8
RKipper2000 <A01283516@itesm.mx>
2023-04-25 09:28:47 -0600

    push 1 25/4/23

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,42 +1,38 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 import './Login.css'
 
 function Login(){
 
-    //const[logData, updateLogData] = useState("");
+    const navigate = useNavigate(); // hook para navegar entre vistas    
 
-    const dataDavid = {
-        usr: "david",
-        psswrd : "martinez"
-    };
-    
-
-    function isUser(user, query){
-        return user.usr === query
-    }
 
     const userArray = [{usr:"david", psswrd:"martinez"},{usr:"leonardo",psswrd:"davinci"},{usr:"keanu", psswrd:"reeves"}]
 
     function displayLoginInfo(){
         // datos de prueba de login
         let usuario = {usr:"", psswrd:""}
-        let usrQuery = {usr:"", psswrd:""}
-        usuario.usr = document.getElementById("username").value;
-        usuario.psswrd = document.getElementById("pass").value;
+        let usr = ""
+        let psswrd = ""
+        usr = document.getElementById("username").value;
+        psswrd = document.getElementById("pass").value;
         //usuario = userArray.find(x => x.usr === usuario.usr);
-        usrQuery = userArray.find(x => x.usr === usuario.usr)
+        //usrQuery = userArray.find(x => x.usr === usr)
 
-        if (usrQuery  !== undefined){
-            if (usuario.usr === usrQuery.usr && usuario.psswrd === usrQuery.psswrd){
-                alert("usuario: " + usuario.usr + "\npassword: " + usuario.psswrd)
+        if (userArray.find(x => x.usr === usr)){ // revisa el userArray, cambiar despues
+            if (psswrd === userArray.find(x => x.usr === usr).psswrd){
+                usuario.usr = usr;
+                usuario.psswrd = psswrd
+                //alert("usuario: " + usuario.usr + "\npassword: " + usuario.psswrd)
+                navigate('/usuarios')
             } else {
-                document.getElementById("errorMsg").innerHTML = "El usuario o contraseña son incorrectos"
+                document.getElementById("error-msg").innerHTML = "El usuario o contraseña son incorrectos"
             }
             
         }else{
             //alert("El usuario o la contraseña son incorrectos\nPor favor verifica tu información")
-            document.getElementById("errorMsg").innerHTML = "El usuario o contraseña son incorrectos"
+            document.getElementById("error-msg").innerHTML = "El usuario o contraseña son incorrectos"
         }
     }
 
@@ -53,7 +49,7 @@ function Login(){
                     <input type="password" id="pass" name="contrasena"></input><br></br>
                 </form>
                 <button onClick={displayLoginInfo}>Ingresar</button><br></br>
-                <p id="errorMsg"></p>
+                <p id="error-msg"></p>
             </div>
             
             <img src="../../../../TerniumLoginImg.jpeg" id="login-image" alt="logo"></img>
