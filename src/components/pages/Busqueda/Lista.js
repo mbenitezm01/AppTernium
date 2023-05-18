cc34521fede2e8209f6271cfb122283bca7c3dff
mbenitezm01 <103158739+mbenitezm01@users.noreply.github.com>
2023-05-18 11:32:17 -0600

    Avance en estilo de listado

--- src/components/pages/Busqueda/Lista.js
+++ src/components/pages/Busqueda/Lista.js
@@ -114,12 +114,12 @@ function Lista({ data, filtersState, handleSubmit }) {
                 id: 'key_talent',
                 Header: 'Key Talent',
                 accessor: 'key_talent',
-                Cell: ({value}) => value ? <AiFillCheckCircle className='lista-key' /> : '',
+                Cell: ({ value }) => value ? <AiFillCheckCircle className='lista-key' /> : '',
                 sortType: (rowA, rowB, columnId) => {
                     const a = rowA.original[columnId];
                     const b = rowB.original[columnId];
                     return a === b ? 0 : (a ? -1 : 1);
-                  }
+                }
             },
         ],
         []
@@ -134,23 +134,26 @@ function Lista({ data, filtersState, handleSubmit }) {
             manualFilters: false,
             filterTypes: React.useMemo(
                 () => ({
-                  num: (rows, id, filterValue) => {
-                    return rows.filter((row) => {
-                        return filterValue === '' || String(row.values[id]) === filterValue})
-                  },
-                  range: (rows, id, filterValue) => {
-                    return rows.filter((row) => {
-                        return filterValue === '' || String(row.values[id]) === filterValue})
-                  },
+                    num: (rows, id, filterValue) => {
+                        return rows.filter((row) => {
+                            return filterValue === '' || String(row.values[id]) === filterValue
+                        })
+                    },
+                    range: (rows, id, filterValue) => {
+                        return rows.filter((row) => {
+                            return filterValue === '' || String(row.values[id]) === filterValue
+                        })
+                    },
                 }),
                 []
-              ),
+            ),
         },
-        
+
         useFilters,
         useSortBy,
         usePagination,
         useRowSelect,
+        useSticky,
         hooks => {
             hooks.visibleColumns.push(columns => [
                 {
@@ -170,7 +173,7 @@ function Lista({ data, filtersState, handleSubmit }) {
             ])
         },
 
-        
+
     )
 
     const {
@@ -229,7 +232,7 @@ function Lista({ data, filtersState, handleSubmit }) {
             id: 'key_talent',
             value: filtersState.key,
         },
-    ])
+        ])
     }
 
     useEffect(() => {
@@ -237,8 +240,9 @@ function Lista({ data, filtersState, handleSubmit }) {
     }, [filtersState]);
 
     return (
-        <div className="lista-bg">
-            {/* Te dice las paginas
+        <>
+            <div className="lista-bg">
+                {/* Te dice las paginas
             <pre>
                 <code>
                     {JSON.stringify(
@@ -255,115 +259,115 @@ function Lista({ data, filtersState, handleSubmit }) {
                 </code>
             </pre>
             */}
-            <table {...getTableProps()} className='table-container'>
-                <thead>
-                    {// Loop over the header rows
-                        headerGroups.map(headerGroup => (
-                            // Apply the header row props
-                            <tr {...headerGroup.getHeaderGroupProps()}>
-                                {// Loop over the headers in each row
-                                    headerGroup.headers.map(column => (
-                                        // Apply the header cell props
-                                        <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                                            {// Render the header
-                                                column.render('Header')}
-                                            <span>
-                                                {column.isSorted
-                                                    ? column.isSortedDesc
-                                                        ? '🔻'
-                                                        : '🔺'
-                                                    : ''}
-                                            </span>
-                                        </th>
-                                    ))}
-                            </tr>
-                        ))}
-                </thead>
-                {/* Apply the table body props */}
-                <tbody {...getTableBodyProps()}>
-                    {// Loop over the table rows
-                        page.map((row, i) => {
-                            // Prepare the row for display
-                            prepareRow(row)
-                            return (
-                                // Apply the row props
-                                <tr {...row.getRowProps()} onClick={() => onRowClick(row.original.cet)} style={{ cursor: 'pointer' }}>
-                                    {// Loop over the rows cells
-                                        row.cells.map(cell => {
-                                            // Apply the cell props
-                                            return (
-                                                <td {...cell.getCellProps()}>
-                                                    {// Render the cell contents
-                                                        cell.render('Cell')}
-                                                </td>
-                                            )
-                                        })}
-                                </tr>
-                            )
-                        })}
-                </tbody>
-            </table>
-            <div className='pagination'>
-                <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-                    {'<<'}
-                </button>{' '}
-                <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-                    {'<'}
-                </button>{' '}
-                <button onClick={() => nextPage()} disabled={!canNextPage}>
-                    {'>'}
-                </button>{' '}
-                <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-                    {'>>'}
-                </button>{' '}
-                <span>
-                    Page{' '}
-                    <strong>
-                        {pageIndex + 1} of {pageOptions.length}
-                    </strong>{' '}
-                </span>
-                <span>
-                    | Go to page:{' '}
-                    <input
-                        type="number"
-                        defaultValue={pageIndex + 1}
+                <div className='table-wrapper'>
+                    <table {...getTableProps()} className='table-container table sticky'>
+                        <thead>
+                            {// Loop over the header rows
+                                headerGroups.map(headerGroup => (
+                                    // Apply the header row props
+                                    <tr {...headerGroup.getHeaderGroupProps()}>
+                                        {// Loop over the headers in each row
+                                            headerGroup.headers.map(column => (
+                                                // Apply the header cell props
+                                                <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                                    {// Render the header
+                                                        column.render('Header')}
+                                                    <span
+                                                        className={`table-sorting-indicator ${column.isSorted ? 'active' : ''
+                                                            } ${column.isSortedDesc ? 'down' : 'up'}`}
+                                                    ></span>
+                                                </th>
+                                            ))}
+                                    </tr>
+                                ))}
+                        </thead>
+                        {/* Apply the table body props */}
+                        <tbody {...getTableBodyProps()}>
+                            {// Loop over the table rows
+                                page.map((row, i) => {
+                                    // Prepare the row for display
+                                    prepareRow(row)
+                                    return (
+                                        // Apply the row props
+                                        <tr {...row.getRowProps()} onClick={() => onRowClick(row.original.cet)} style={{ cursor: 'pointer' }}>
+                                            {// Loop over the rows cells
+                                                row.cells.map(cell => {
+                                                    // Apply the cell props
+                                                    return (
+                                                        <td {...cell.getCellProps()} title={cell.value}>
+                                                            {// Render the cell contents
+                                                                cell.render('Cell')}
+                                                        </td>
+                                                    )
+                                                })}
+                                        </tr>
+                                    )
+                                })}
+                        </tbody>
+                    </table>
+                </div>
+                <div className='pagination'>
+                    <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                        {'<<'}
+                    </button>{' '}
+                    <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                        {'<'}
+                    </button>{' '}
+                    <button onClick={() => nextPage()} disabled={!canNextPage}>
+                        {'>'}
+                    </button>{' '}
+                    <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                        {'>>'}
+                    </button>{' '}
+                    <span>
+                        Page{' '}
+                        <strong>
+                            {pageIndex + 1} of {pageOptions.length}
+                        </strong>{' '}
+                    </span>
+                    <span>
+                        | Go to page:{' '}
+                        <input
+                            type="number"
+                            defaultValue={pageIndex + 1}
+                            onChange={e => {
+                                const page = e.target.value ? Number(e.target.value) - 1 : 0
+                                gotoPage(page)
+                            }}
+                            style={{ width: '100px' }}
+                        />
+                    </span>{' '}
+                    <select
+                        value={pageSize}
                         onChange={e => {
-                            const page = e.target.value ? Number(e.target.value) - 1 : 0
-                            gotoPage(page)
+                            setPageSize(Number(e.target.value))
                         }}
-                        style={{ width: '100px' }}
-                    />
-                </span>{' '}
-                <select
-                    value={pageSize}
-                    onChange={e => {
-                        setPageSize(Number(e.target.value))
-                    }}
-                >
-                    {[10, 20, 30, 40, 50].map(pageSize => (
-                        <option key={pageSize} value={pageSize}>
-                            Show {pageSize}
-                        </option>
-                    ))}
-                </select>
-                {/* Te dice los ids
+                    >
+                        {[10, 20, 30, 40, 50].map(pageSize => (
+                            <option key={pageSize} value={pageSize}>
+                                Show {pageSize}
+                            </option>
+                        ))}
+                    </select>
+                    {/* Te dice los ids
             <pre>
-                <code>
+            <code>
                     {JSON.stringify(
                         {
                             selectedRowIds: selectedRowIds,
                             'selectedFlatRows[].original': selectedFlatRows.map(
                                 d => d.original
-                            ),
-                        },
-                        null,
-                        2
-                    )}
-                </code>
-            </pre>
-        */}
+                                ),
+                            },
+                            null,
+                            2
+                            )}
+                            </code>
+                            </pre>
+                        */}
+                </div>
             </div>
-        </div>
+        </>
 
     )
 }
