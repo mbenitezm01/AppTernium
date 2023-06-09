7884e98f5ae0625a3b2afe9412f26478a123b6fe
PabloGZ9 <pablo.9gz@gmail.com>
2023-06-08 23:28:19 -0500

    Avance

--- src/components/pages/Comparacion/Comparacion.js
+++ src/components/pages/Comparacion/Comparacion.js
@@ -21,10 +21,10 @@ function Comparacion(){
         const data = {
             refreshToken: sessionStorage.getItem('refreshToken')
         }
-        const response1 = await axios.get(`${process.env.REACT_APP_API_HOST}/api/info-empleado/${params.get('ficha1')}`, data, config);
-        if(response1.data.accessToken !== null) sessionStorage.setItem('accessToken', response.data.accessToken);
-        const response2 = await axios.get(`${process.env.REACT_APP_API_HOST}/api/info-empleado/${params.get('ficha2')}`, data, config);
-        if(response2.data.accessToken !== null) sessionStorage.setItem('accessToken', response.data.accessToken);
+        const response1 = await axios.post(`${process.env.REACT_APP_API_HOST}/api/info-empleado/${params.get('ficha1')}`, data, config);
+        if(response1.data.accessToken !== null) sessionStorage.setItem('accessToken', response1.data.accessToken);
+        const response2 = await axios.post(`${process.env.REACT_APP_API_HOST}/api/info-empleado/${params.get('ficha2')}`, data, config);
+        if(response2.data.accessToken !== null) sessionStorage.setItem('accessToken', response2.data.accessToken);
         if(response1.data.encontrado && response2.data.encontrado){
             setInfo([response1.data, response2.data]);
         }
